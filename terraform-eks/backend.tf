eefb09d46d502019f2fff55df1d187a5923d1e69
Vineeth <113631587+vineethsankre@users.noreply.github.com>
2026-03-25 18:07:54 +0530

    Update backend.tf

--- terraform-eks/backend.tf
+++ terraform-eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "eks-s3-snakegame-bucket"
+    bucket         = "eks-s3-snakegame-app-bucket "
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
     use_lockfile   = true
